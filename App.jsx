c2f553779cc69657737b14e9b949d0f3d1edead6
Guilherme Mendes Barnabe <guilherme.barnabe@grupointegrado.br>
2023-09-11 14:44:16 -0300

    Retirada de arquivos inuteis

--- App.jsx
+++ App.jsx
@@ -1,8 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './src/screens/LoginScreen';
-import HomeScreen from './src/screens/Home';
-import FlagScreen from './src/screens/Flag';
 import Listagem from './src/screens/Listagem';
 
 export default function App() {
