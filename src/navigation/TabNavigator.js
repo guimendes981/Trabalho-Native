586bbf4e899bce504d5e3b6a25a573982c09a38f
Guilherme Mendes Barnabe <guilherme.barnabe@grupointegrado.br>
2023-09-18 13:55:16 -0300

    Icons

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -4,16 +4,53 @@ import LoginForm from "../components/LoginForm";
 import Home from "../components/Home";
 import RandomCard from "../components/RandomCard";
 import CardSets from "../components/CardSets";
+import { AntDesign } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Login" component={LoginForm} />
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="RandomCard" component={RandomCard} />
-      <Tab.Screen name="CardSets" component={CardSets} />
+      <Tab.Screen
+        name="Login"
+        component={LoginForm}
+        options={{
+          tabBarLabel: "Login",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="user" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="RandomCard"
+        component={RandomCard}
+        options={{
+          tabBarLabel: "Random Card",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="question" size={24} color="black" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="CardSets"
+        component={CardSets}
+        options={{
+          tabBarLabel: "Card Sets",
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="book" size={24} color="black" />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
